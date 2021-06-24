f4b027e44a68fc0b685196f94cd85fee96799f51
castellon123 <63824225+castellon123@users.noreply.github.com>
2021-06-24 00:22:29 -0600

    Add files via upload

---
+++ DarlySanchez/DarlySanchez/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace DarlySanchez
+{
+    partial class CalculoIntereses
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(198, 393);
+            this.btncalcular.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(242, 85);
+            this.btncalcular.TabIndex = 3;
+            this.btncalcular.Text = "Calculo";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(199, 13);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 364);
+            this.listBox1.TabIndex = 2;
+            // 
+            // CalculoIntereses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 511);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.listBox1);
+            this.Name = "CalculoIntereses";
+            this.Text = "Calculo De Intereses";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ DarlySanchez/DarlySanchez/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DarlySanchez
+{
+    public partial class CalculoIntereses : Form
+    {
+        public CalculoIntereses()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            {
+                deposito();
+            }
+
+
+            private void deposito()
+            {
+                int interes;
+                int deposito;
+                int resultado;
+
+                listBox1.Items.Add("Enero");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Febrero");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 2);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Marzo");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 3);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Abril");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 4);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Mayo");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 5);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Junio");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 6);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Julio");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 7);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Agosto");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 8);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Septiembre");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 9);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Octubre");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 10);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Noviembre");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 11);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+                listBox1.Items.Add("Diciembre");
+                deposito = 200000;
+                interes = Convert.ToInt32(deposito * 0.015 * 12);
+                resultado = interes;
+                listBox1.Items.Add(interes);
+            }
+        }
+    }
+
+
+  
